57b4f3be63c25e63734e6e39b8f2628327eeb39c
lilymadrigal <131197581+lilymadrigal@users.noreply.github.com>
2023-10-22 22:07:44 -0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+        let name, email, number, message;
+
+        $("#fullname").change(function () {
+                name = this.value;
+        });
+
+        $("#email").change(function () {
+                email = this.value;
+        });
+
+        $("#number").change(function () {
+                number = this.value;
+        });
+    
+        $("#message").change(function () {
+                message = this.value;
+        });
+    
+        $("form").submit(function (event) {
+                event.preventDefault(); 
+    
+                check_Input(name, email, number, message);
+            });
+});
+
+function check_Input(name, email, number, message) {
+        let invalidInput = 0;
+
+        if(!name) {
+                invalidInput++;
+                alert("Please enter your name.")
+        } 
+        if(!email) {
+                invalidInput++;
+                alert("Please enter your phone number.")
+        } 
+        if(!number) {
+                invalidInput++;
+                alert("Please enter your email address.")
+        } 
+        if(!message) {
+                invalidInput++;
+                alert("Please enter a message.")
+        }
+        if(invalidInput === 0) {
+                $('#formSubmit').html('<p>Form Received! You Will Hear From Us Shortly.</p>');
+        }
+}
